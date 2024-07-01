cb2cda7e563bb76f91b8b884c22956e3f100b38a
Rafli Setiawan <dev.raflisetiawan@gmail.com>
2024-07-01 19:57:05 +0700

    aksdjaskld

--- src/composables/api/index.ts
+++ src/composables/api/index.ts
@@ -7,7 +7,8 @@ import { Cookies } from 'quasar';
  * @returns {AxiosInstance} The axios instance with authorization headers.
  */
 const useApiWithAuthorization = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'saya-uisi-api.qcbkp.tech/api/',
+  // baseURL: 'http://localhost:8000/api/',
   headers: {
     Authorization: `Bearer ${Cookies.get('token')}`,
   },
